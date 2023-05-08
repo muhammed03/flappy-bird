a550dfd166c1ad0ff961dfa27774e935c86f1c8f
Muhammed Abdrahman <muhammed03amin@gmail.com>
2023-05-08 17:10:15 +0600

    feat: add params for instance of class game

--- src/components/Game/game.js
+++ src/components/Game/game.js
@@ -1,8 +1,24 @@
-import config from "../../constants/config";
+import Config from "../../constants/config";
 class Game {
     constructor() {
         // config
-        this._config = config;
+        this._config = Config;
+
+        this._canvasListener = null;
+
+        // canvas
+        this._canvas = document.getElementById(this._config.canvas.canvasId);
+        this._restartBtn = document.getElementById(this._config.canvas.restartBtnId);
+        this._canvas.width = this._config.canvas.width;
+        this._canvas.height = this._config.canvas.height;
+
+        // size of the game
+        this.height = this._config.canvas.height;
+        this.width = this._config.canvas.width;
+
+        // rating
+        this._score = 0;
+        this._record = !localStorage.getItem("record") ? 0 : localStorage.getItem("record");
     }
 }
 export default new Game();
