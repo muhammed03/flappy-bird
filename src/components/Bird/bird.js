1d8a408fb7cda8c04052c53b90d57b390599bd14
Muhammed Abdrahman <muhammed03amin@gmail.com>
2023-05-08 19:14:21 +0600

    feat: add method crash for bird entity

--- src/components/Bird/bird.js
+++ src/components/Bird/bird.js
@@ -13,6 +13,23 @@ export default class Bird extends Base {
         super.update(delta);
 
         this._physicsEngine.update(this, delta);
+
+        this.#crash();
+    }
+
+    #crash() {
+        // bird ground collision condition
+        // const conditionForGround = this.y + this.height >= this._game.height;
+
+        // condition for the bird not to fly above the ceiling
+        if (this.y < 0) {
+            this.y = 0;
+        }
+
+        // condition for end game
+        // if (conditionForGround) {
+        //   this._game.gameOver();
+        // }
     }
 
     // метод подлёта
